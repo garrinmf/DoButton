e1447e368cd78ae44f9bc20c9ad494c6f83cd79d
Mark Garringer <mark@thehubmedia.com>
2015-06-26 15:05:50 -0400

    Changed name

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,22 @@
-var data = window.localStorage.getItem('data') ? JSON.parse(window.localStorage.getItem('data')) : {events: []};
-
-Pebble.addEventListener('showConfiguration', function() {
-  console.log('show config');
-  Pebble.openURL('http://garrinmf.github.io/DoButton/do_configuration_v2.html?data=' + encodeURIComponent(JSON.stringify(data)));
-});
-
-Pebble.addEventListener('webviewclosed', function(e) {
-  data = JSON.parse(decodeURIComponent(e.response));
-  console.log('Configuration window returned: ', JSON.stringify(data));
-  window.localStorage.setItem('data', JSON.stringify(data))
-  setupMain();
-});
-
+/*jslint browser: true, white: true*/
+/*global Pebble*/
 var UI = require('ui'),
-  main;
-
-setupMain();
+    main, i,
+    data = window.localStorage.getItem('data') ? JSON.parse(window.localStorage.getItem('data')) : {events: []};
 
 function setupMain() {
   if (main !== undefined) {
     main.hide();
   }
 
-  if(data.key != undefined && data.events.length > 0) {
+  if (data.key != undefined && data.events.length > 0) {
     var config = {
       sections: [{
         items: []
       }]
     };
 
-    for(var i = 0; i < data.events.length; i++){
+    for (i = 0; i < data.events.length; i++) {
       config.sections[0].items.push({
         title: data.events[i].name
       });
@@ -37,26 +24,26 @@ function setupMain() {
 
     main = new UI.Menu(config);
 
-    main.on('select', function(e) {
+    main.on('select', function (e) {
       var name = data.events[e.itemIndex].name,
           event = data.events[e.itemIndex].event,
           values = data.events[e.itemIndex].values || [],
-          props = "?";
+          props = "?", tmpCard;
 
-      for(var i = 0; i < values.length; i++) {
-        props += values[i].name '=' + encodeURIComponent(values[i].value) + "&";
+      for (i = 0; i < values.length; i++) {
+        props += values[i].name + '=' + encodeURIComponent(values[i].value) + "&";
       }
 
       console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
       console.log('payload:' + props);
 
       var req = new XMLHttpRequest();
-      req.open('POST', 'https://maker.ifttt.com/trigger/'+ event +'/with/key/' + data.key + props);
+      req.open('POST', 'https://maker.ifttt.com/trigger/' + event + '/with/key/' + data.key + props);
 
       req.onload = function(e) {
         console.log("Status: " + JSON.stringify(req));
         if (req.status == 200) {
-          var tmpCard = new UI.Card({
+          tmpCard = new UI.Card({
             title: name,
             body: "Success!"
           });
@@ -67,7 +54,7 @@ function setupMain() {
         }
 
         if (req.status != 200) {
-          var tmpCard = new UI.Card({
+          tmpCard = new UI.Card({
             title: name,
             body: "Failed!: " + req.status
           });
@@ -93,3 +80,17 @@ function setupMain() {
     main.show();
   }
 }
+
+Pebble.addEventListener('showConfiguration', function () {
+  console.log('show config');
+  Pebble.openURL('http://garrinmf.github.io/DoButton/do_configuration_v2.html?data=' + encodeURIComponent(JSON.stringify(data)));
+});
+
+Pebble.addEventListener('webviewclosed', function (e) {
+  data = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration window returned: ', JSON.stringify(data));
+  window.localStorage.setItem('data', JSON.stringify(data));
+  setupMain();
+});
+
+setupMain();
